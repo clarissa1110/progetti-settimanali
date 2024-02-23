c89c2fba70a094055dbb6629c6adc1c08e7954a0
clarissa1110 <clarissapiovesan@gmail.com>
2024-02-24 00:24:39 +0100

    commit finale - risoluzione ottimale 1200px

--- sett-5-progetto/assets/js/script.js
+++ sett-5-progetto/assets/js/script.js
@@ -1,13 +1,37 @@
-let header = document.getElementById('header');
-let heroHeight = header.offsetTop;
 
+window.addEventListener('scroll', function () {
+    let hero = document.getElementById('hero');
+    let heroHeight = hero.offsetHeight;
+    let button = document.getElementById('start');
 
-function cambiaColore() {
-    if (window.scrollY >= heroHeight) {
-        header.style.backgroundColor('white')
-    } else {
-        header.style.backgroundColor('#ffc017')
+    function cambiaColore() {
+
+        if (window.scrollY >= heroHeight) {
+            header.classList.add("headerScroll");
+        } else {
+            header.classList.remove("headerScroll");
+        }
     }
-}
 
-cambiaColore();
+    /*function cambiaButton() {
+        if (window.scrollY >= heroHeight) {
+            button.classList.add("buttonScroll");
+        } else {
+            button.classList.remove("buttonScroll");
+        }
+    }*/
+
+    function cambiaButton() {
+
+        if (window.scrollY >= heroHeight) {
+            button.style.backgroundColor = "#1a8917";
+        } else {
+            button.style.backgroundColor = "#191919";
+        }
+    }
+
+    cambiaColore();
+    cambiaButton();
+});
+
+
