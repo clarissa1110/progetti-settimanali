8bef0b4bcbfbe38a457963391b29134937f7cae7
clarissa1110 <clarissapiovesan@gmail.com>
2024-04-07 17:23:39 +0200

    register ok

--- sett-11-progetto/src/app/app.module.ts
+++ sett-11-progetto/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { Route, RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -11,6 +12,8 @@ import { MoviesComponent } from './components/movies/movies.component';
 import { MoviesDetailsComponent } from './components/movies-details/movies-details.component';
 import { UserProfileComponent } from './components/user-profile/user-profile.component';
 import { UsersListComponent } from './components/users-list/users-list.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
 
 const routes: Route[] = [
   {
@@ -36,6 +39,14 @@ const routes: Route[] = [
   {
     path: 'favourites',
     component: FavouritesComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
   }
 ]
 
@@ -48,12 +59,16 @@ const routes: Route[] = [
     MoviesComponent,
     MoviesDetailsComponent,
     UserProfileComponent,
-    UsersListComponent
+    UsersListComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(routes),
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
